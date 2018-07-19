a8c74a93a04a3d9cc534c89fde92fc4418e50d3c
Volha Valasevich <volha_valasevich@epam.com>
2018-07-19 10:52:55 +0300

    Initial commit

---
+++ test/e2e/config/config.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const path = require('path');
+const yargs = require('yargs').argv;
+
+exports.config = {
+    allScriptsTimeout: 200000,
+    getPageTimeout: 200000,
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: [path.resolve('./test/e2e/features/*.feature')],
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    ignoreUncaughtExceptions: true,
+    browser: 'chrome',
+    disableChecks: true,
+    cucumberOpts: {
+        require: [path.resolve('./test/e2e/step_definitions/**/*.js')],
+        ignoreUncaughtExceptions: true,
+        format: 'json:./reports/report.json',
+        tags: yargs.tag||'@epam'
+    },
+    onPrepare: () => {
+        browser.ignoreSynchronization = true;
+        browser.manage().window().maximize();
+    }
+};
